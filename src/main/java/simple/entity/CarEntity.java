4e2896212d6c1c7ed20428eade2d1ccb77be1f73
tapanbankercode <tapan@tcloudplus.com>
2018-09-22 14:38:55 -0400

    Getters and setters

--- src/main/java/simple/entity/CarEntity.java
+++ src/main/java/simple/entity/CarEntity.java
@@ -8,6 +8,22 @@ public class CarEntity {
     private String name;
     private int year;
     
+    public String getName() {
+ 		return name;
+ 	}
+
+ 	public void setName(String name) {
+ 		this.name = name;
+ 	}
+
+ 	public int getYear() {
+ 		return year;
+ 	}
+
+ 	public void setYear(int year) {
+ 		this.year = year;
+ 	}
+ 	
     public CarEntity duplicate(final CarEntity entity) {
         CarEntity instance = new CarEntity();
         instance.setName(name);
